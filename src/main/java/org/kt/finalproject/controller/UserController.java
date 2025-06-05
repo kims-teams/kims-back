688e8783d1047377b00c77171f4aaed04702b72e
Boaz11Jachin <sheepo84k@gmail.com>
2025-06-05 11:58:25 +0900

    모든 유저 목록, 유저 추가 api

--- src/main/java/org/kt/finalproject/controller/UserController.java
+++ src/main/java/org/kt/finalproject/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.kt.finalproject.entity.User;
 import org.kt.finalproject.repository.UserRepository;
 import org.kt.finalproject.request.UserDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,10 @@ public class UserController {
 
     private final UserRepository userRepository;
 
+    @Value("${secret}")
+    private String secret;
+
+
     @GetMapping
     public ResponseEntity<?> getUserHandle() {
         List<User> userList = userRepository.findAll();
@@ -31,7 +36,20 @@ public class UserController {
     public ResponseEntity<?> postUserHandle (@RequestBody @Valid UserDto dto
                                                 , BindingResult result) {
 
-        return null;
+        if (result.hasErrors()){
+            return ResponseEntity.status(400).body(null);
+        }
+
+        User user = User.builder()
+                .businessEmail(dto.getBusinessEmail())
+                .password(dto.getPassword())
+                .name(dto.getName())
+                .position(dto.getPosition()).build();
+
+        userRepository.save(user);
+
+
+        return ResponseEntity.status(201).body(user);
     }
 
 
--- src/main/java/org/kt/finalproject/entity/user.java
+++
@@ -1,4 +0,0 @@
-package org.kt.finalproject.entity;
-
-public class user {
-}
