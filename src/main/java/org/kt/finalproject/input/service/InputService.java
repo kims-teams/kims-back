63a06470705dbdc78e97287921082e7f23480c0c
soolyn <soolyn4867@gmail.com>
2025-06-11 12:43:15 +0900

    start : input service

---
+++ src/main/java/org/kt/finalproject/input/service/InputService.java
@@ -0,0 +1,11 @@
+package org.kt.finalproject.input.service;
+
+import java.util.List;
+
+public class InputService {
+
+    public List<Object> saveInputData(List<Object> item, String dataType){
+
+        return null;
+    }
+}
